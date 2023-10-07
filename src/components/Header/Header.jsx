4474712b0e8c5c52992300ed6d5672d5338f3792
Manuel Jimena García <Manuel.jimena29@gmail.com>
2023-10-07 21:27:11 +0200

    end project

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,20 @@
 import "./Header.css";
-import { useState } from 'react';
 
 const Header = ({ logo, logoAlt }) => {
-  const [menuOpen, setMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
+  
+  const menuBtnClick = () => {
+    const menuBtn = document.querySelector('#menu-btn');
+    const navbar = document.querySelector('.header .navbar');
+    menuBtn.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+  }
 
   return (
     <header className="header" translate="no">
-      <div
-        id="menu-btn"
-        className={`fas ${menuOpen ? 'fa-times' : 'fa-bars'} icons`}
-        onClick={toggleMenu}
-      ></div>
+      <div id="menu-btn" className="fas fa-bars icons" onClick={menuBtnClick}></div>
       <div className="nothing" id="null"></div>
 
-      <nav className={`navbar ${menuOpen ? 'active' : ''}`}>
+      <nav className="navbar">
         <a href="#home">inicio</a>
         <a href="#menu">burguers</a>
         <a href="#entrantes">entrantes</a>
@@ -26,6 +23,7 @@ const Header = ({ logo, logoAlt }) => {
         <a href="#menús">menús</a>
         <a href="#contact">contacto</a>
       </nav>
+      <a href="#" className="no-action-link"></a>
       <a href="#" className="no-action-link"></a>
       <a href="#home" className="logo">
         <img src={logo} alt={logoAlt} />
