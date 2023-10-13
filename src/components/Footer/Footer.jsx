b62b0a8dc4dd185051f4d0703b89da9c7b2b82d6
Manuel Jimena García <Manuel.jimena29@gmail.com>
2023-10-13 18:51:39 +0200

    refact footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,9 +3,11 @@ import "./Footer.css"
 const Footer = () => {
 
     return (
+        <footer>
         <div className="standard-dialog center">
             <h2 className="dialog-text">@Manuel Jimena - 2023 - <a href="https://api.nasa.gov/">https://api.nasa.gov/</a></h2>
         </div>
+        </footer>
     );
 };
 
