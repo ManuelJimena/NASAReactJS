c03b96534f6140208b3d1b7b9a6d8fe8be47cb1b
Manuel Jimena García <Manuel.jimena29@gmail.com>
2023-10-14 00:05:22 +0200

    refact navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import "./Header.css";
 import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Header = ({ logo, logoAlt }) => {
   
@@ -13,18 +14,20 @@ const Header = ({ logo, logoAlt }) => {
   return (
     <header className="header" translate="no">
       <div id="menu-btn" className="fas fa-bars icons" onClick={menuBtnClick}></div>
-      <div className="nothing" id="null"></div>
+      <a href="#" className="no-action-link">O</a>
       <nav className="navbar">
+      
         <NavLink to="">Home</NavLink>
         <NavLink to="apod">Apod</NavLink>
         <span className="space"></span>
         <NavLink to="gallery">Gallery</NavLink>
         <NavLink to="mars">Mars</NavLink>
+        
       </nav>
-      <a href="#" className="no-action-link"></a>
-      <a href="#home" className="logo">
-        <img src={logo} alt={logoAlt} />
-      </a>
+      <a href="#" className="no-action-link">O</a>
+      <div className="logo">
+        <Link to="/"><img src={logo} alt={logoAlt} /></Link>
+      </div>
     </header>
   );
 };
